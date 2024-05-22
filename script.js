780bc323512fc547c293a710e390e124bd743b3b
josue ortiz <ortizjd0036@gmail.com>
2024-05-22 11:16:01 -0400

    feat(style, script):[add funtionality display amount, display intereest, in , out and add functionality for transfer monet between accounts]

--- script.js
+++ script.js
@@ -62,34 +62,6 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 
-
-const displayMovements = function(movements){
-  movements.forEach(function(mov , i){
-
-    // containerMovements.innerHTML = '';
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
-
-    const html = `<div class="movements__row">
-    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__date">3 days ago</div>
-    <div class="movements__value">${mov}</div>
-  </div>
-  `;
-  containerMovements.insertAdjacentHTML('afterbegin' , html);
-  });
-} 
-displayMovements(account1.movements);
-
-/***calc Print Balance Funtion */
-const calcPrintBalance = function(movement){
-  const balance = movement.reduce(function(acc , mov){
-    return acc + mov;
-  } , 0);
-  labelBalance.textContent = `${balance} EUR`;
-}
-calcPrintBalance(account1.movements);
-/***calc Print Balance Funtion */
-
 /***Print Deposit Funtion */
 // const prinDeposit = function(movement){
 //   const deposit = movement.filter(function(mov){
@@ -104,27 +76,6 @@ calcPrintBalance(account1.movements);
 // prinDeposit(account1.movements);
 
 
-/********calc Display Summary */
-const calcDisplaySummary = function(movement){
-  const totalDeposit = movement.filter(function(mov){
-    return mov > 0;
-  }).reduce(function(acc , mov){
-    return acc + mov;
-  } , 0);
-  labelSumIn.textContent = `${totalDeposit} €`;
-
-  
-  const totalWithdrawal = movement.filter(function(mov){
-    return mov < 0;
-  }).reduce(function(acc , mov){
-    return acc + mov
-  } , 0);
-  labelSumOut.textContent = `${Math.abs(totalWithdrawal)} € `;
-
-}
-calcDisplaySummary(account1.movements);
-/********calc Display Summary */
-
 
 const createUserName = function(accs){ 
   accs.forEach(function(acc){
@@ -134,8 +85,153 @@ const createUserName = function(accs){
   })
 };
 
-
 createUserName(accounts);
 
 
+let currentAccount;
+
+btnLogin.addEventListener('click' , function(e){
+  //Prevent form from submitting
+  e.preventDefault()
+  
+//  console.log(inputLoginUsername);
+    currentAccount = accounts.find(function(acco){
+      return acco.userName === inputLoginUsername.value;
+  });
+   if(currentAccount?.pin === Number(inputLoginPin.value)){
+    console.log('Login');
+    //Display UI and message
+    labelWelcome.textContent = `Welcome back ${currentAccount.owner}`;
+    containerApp.style.opacity = 100;
+
+    //Display movements 
+    displayMovements(currentAccount.movements);
+    // Display balance
+    calcPrintBalance(currentAccount.movements);
+
+    // Displyas summary
+    calcDisplaySummary(currentAccount);
+    
+    //Clear input fileds
+     inputLoginPin.value = '';
+     inputLoginPin.value = ''; 
+
+     inputLoginPin.blur();
+
+  }
+   else{
+    containerApp.style.opacity = 0;
+    openModal();
+   }
+});
+
+const displayMovements = function(movements){
+  containerMovements.innerHTML = '';
+  movements.forEach(function(mov , i){
+
+    // containerMovements.innerHTML = '';
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `<div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__date">3 days ago</div>
+    <div class="movements__value">${mov}</div>
+  </div>
+  `;
+  containerMovements.insertAdjacentHTML('afterbegin' , html);
+  });
+} 
+
+/***calc Print Balance Funtion */
+const calcPrintBalance = function(movement){
+  const balance = movement.reduce(function(acc , mov){
+    return acc + mov;
+  } , 0);
+  labelBalance.textContent = `${balance} EUR`;
+}
+
+/***calc Print Balance Funtion */
+
+
+/********calc Display Summary */
+const calcDisplaySummary = function(account){
+  // console.log(account);
+  const totalDeposit = account.movements.filter(function(mov){
+    return mov > 0;
+  }).reduce(function(acc , mov){
+    return acc + mov;
+  } , 0);
+  labelSumIn.textContent = `${totalDeposit} €`;
+
+
+  const totalWithdrawal = account.movements.filter(function(mov){
+    return mov < 0;
+  }).reduce(function(acc , mov){
+    return acc + mov
+  } , 0);
+  labelSumOut.textContent = `${Math.abs(totalWithdrawal)} € `;
+
+
+  const interest = account.movements.filter(function(mov){
+      return mov > 0;
+  }).map(function(deposit){
+    return (deposit * account.interestRate)/ 100;
+  })
+  .filter(function(inter, i ,arr){
+    return inter >= 1;
+  })
+  .reduce(function(acc , interest ){
+    return acc + interest;
+  },0 );
+  labelSumInterest.textContent = `${interest} €`
+}
+
+/********calc Display Summary */
+
+
+/**show modal */
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+
+const closeModal = function(){
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+const openModal = function(){
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden');
+}
+
+btnCloseModal.addEventListener('click' , closeModal);
+
+document.addEventListener('keydown', function(e){
+  if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+    closeModal();
+  }
+})
+
+
+const transfer = function(transfTo , amount){
+  const accountFind = accounts.find(function(acc){
+    return acc.userName === transfTo
+  });
+  if(accountFind?.userName === transfTo){
+    console.log(`transferencia hecha a ${transfTo}  de ${amount} con exito`);
+  }else{
+    console.log(`tranferecia rechazada`);
+  }
+
+}
+
+
+
+btnTransfer.addEventListener('click' , function(e){
+  e.preventDefault()
+  const userToTransfer = inputTransferTo.value;
+  const amountToTransfer = inputTransferAmount.value;
+  transfer(userToTransfer , amountToTransfer)
+})
 
